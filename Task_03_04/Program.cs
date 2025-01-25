ac1cf00a46afdfc78966326074814ec446344255
Данияр Жандаупов <saltanat.zhandaupova@gmail.com>
2025-01-25 20:26:27 +0500

    выполнена задача 4 урока 3

--- Task_03_04/Program.cs
+++ Task_03_04/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        /*Пользователь вводит в консоли произвольный текст.
+         * После каждого ввода консоль очищается. 
+         * Когда пользователь вводить слово «exit»
+         * или пустую строку – ввод останавливается
+         * и выводиться количество строк 
+         * введенных пользователем.
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //ввод слов, записываемых пользователем
+            Console.WriteLine("осуществите ввод текста построчно");
+            int countOfWords = 0;
+
+            while (true)
+            {
+                string temp = Console.ReadLine();
+                if (temp == "" || temp == "exit")
+                    break;
+                //считаем количество строк
+                countOfWords++;
+                Console.Clear();
+            }
+            Console.WriteLine("вы ввели " + countOfWords + " строк ");
         }
     }
 }
