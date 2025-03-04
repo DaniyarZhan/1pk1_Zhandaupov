7112d842836ae80dc0e39df98da6123c07f92b8b
Данияр Жандаупов <saltanat.zhandaupova@gmail.com>
2025-03-05 00:20:12 +0500

    выполнена задача 7 урока 11

---
+++ Task_11_07/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task_11_07
+{
+    internal class Program
+    {
+        /*Передача массива по ссылке (ref):
+         * Напишите метод, который принимает массив целых чисел
+         * по ссылке и изменяет его элементы, 
+         * увеличивая каждый на 1. Проверьте, 
+         * изменился ли оригинальный массив вне метода.
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[a];
+            Random rnd = new Random();            
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 101);
+            }
+            Massive(ref array);
+            Console.Write("массив вне метода: ");
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// Массив, значения которого увеличились на 1 
+        /// </summary>
+        /// <param name="arr">изменённый массив</param>
+        public static void Massive(ref int[] arr)
+        {
+            Console.Write("массив внутри метода: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]++;
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
